5b0e721fff58b6d1057a08d05145b368deae9ffa
abejaMaja <m.remizowicz@gmail.com>
2022-07-02 11:48:50 +0200

    first commit

---
+++ src/main/java/model/Samochod.java
@@ -0,0 +1,29 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Samochod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String marka;
+    private LocalDate rokProdukcji;
+    @Enumerated(EnumType.STRING)
+    private TypNadwozia typNadwozia;
+    private int iloscMiejsc;
+
+
+
+
+}
---
+++ src/main/java/model/TypNadwozia.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum TypNadwozia {
+    SEDAN,
+    CARBIO,
+    SUV;
+}
